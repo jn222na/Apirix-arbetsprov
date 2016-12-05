7ac31f8db647b276148b4b907ef15b227374477d
jn222na <jn222na@student.lnu.se>
2016-12-05 13:56:52 +0100

    Remove unused files

--- app/src/main/java/com/example/jocke/joakim_nilsson_apirix_arbetsprov/DatabaseOptions.java
+++
@@ -1,7 +0,0 @@
-package com.example.jocke.joakim_nilsson_apirix_arbetsprov;
-
-/**
- * Created by Jocke on 2016-12-03.
- */
-
-public enum DatabaseOptions {SENDCOORDINATES,REGISTERDEVICE,PUSHMESSAGE}
